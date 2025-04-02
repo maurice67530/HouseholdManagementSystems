33df3eaa8c868cc20090c2119bc03e6648f332de
Rotondwa <Rotondwa@MUKWEVHO>
2025-04-02 11:02:42 +0200

    Commiting Feature Notifications

---
+++ Notification/Notification/Module1.vb
@@ -0,0 +1,5 @@
+﻿Module Module1
+
+    Public Const connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Rotondwa\Source\Repos\HouseholdManagementSystems\HMS.accdb"
+End Module
+
---
+++ Notification/Notification/Notifications.vb
@@ -0,0 +1,5 @@
+﻿Imports System.IO
+Imports System.Data.OleDb
+Module Notifications
+    Public Const connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Rotondwa\Source\Repos\HouseholdManagementSystems\HMS.accdb"
+End Module
