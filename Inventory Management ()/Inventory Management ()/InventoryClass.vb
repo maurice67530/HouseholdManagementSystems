a8acbd538356e11515ad57390907aed4902ccccf
KUNDANI TRADING <KUNDANI TRADING@MBUYELO>
2025-04-02 10:13:42 +0200

    commit Inventory

---
+++ Inventory Management ()/Inventory Management ()/InventoryClass.vb
@@ -0,0 +1,16 @@
+﻿Public Class Inventory
+
+    Public Property Name As String
+    Public Property Description As String
+    Public Property Quantity As Integer
+    Public Property Unit As String
+    Public Property Category As String
+    Public Property ReorderLevel As String
+    Public Property Total As Integer
+    Public Property PricePerUnit As String
+    Public Property ExpiryDate As Date
+    Public Property DateAdded As Date
+
+
+End Class
+
--- Inventory Management(MT)/Inventory Management(MT)/Form1.cs
+++ Inventory Management(MT)/Inventory Management(MT)/Form1.cs
@@ -16,5 +16,10 @@ namespace Inventory_Management_MT_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
