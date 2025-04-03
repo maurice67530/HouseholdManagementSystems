b3c1be94a5ed2f05375b9ced72b94778b08b7e64
Rotondwa <Rotondwa@MUKWEVHO>
2025-04-03 11:23:59 +0200

    commiting

--- Notification/Notification/Form1.Designer.vb
+++ Notification/Notification/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,25 +20,25 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(13, 78)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(3, 71)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(200, 20)
+        Me.Label2.Size = New System.Drawing.Size(166, 16)
         Me.Label2.TabIndex = 10
         Me.Label2.Text = "Count Unread Notifications"
         '
@@ -46,29 +46,11 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 24)
+        Me.Label1.Location = New System.Drawing.Point(1, 22)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(222, 29)
         Me.Label1.TabIndex = 8
         Me.Label1.Text = "View Notifications"
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(134, 287)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(112, 35)
-        Me.Button2.TabIndex = 7
-        Me.Button2.Text = "Clear Notification"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(6, 287)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(107, 35)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Mark As Read"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Timer1
         '
@@ -76,20 +58,40 @@ Partial Class Form1
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(6, 121)
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 99)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(240, 145)
+        Me.DataGridView1.Size = New System.Drawing.Size(293, 164)
         Me.DataGridView1.TabIndex = 11
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.SystemColors.Control
+        Me.btnClear.Location = New System.Drawing.Point(160, 273)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(130, 40)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear Notification"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 273)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(125, 40)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Mark As Read "
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(267, 334)
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(311, 349)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -101,7 +103,7 @@ Partial Class Form1
 
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btnClear As Button
     Friend WithEvents Button1 As Button
     Friend WithEvents Timer1 As Timer
     Friend WithEvents DataGridView1 As DataGridView
--- Notification/Notification/Form1.vb
+++ Notification/Notification/Form1.vb
@@ -1,229 +1,169 @@
 ﻿Imports System.IO
 Imports System.Data.OleDb
 Public Class Form1
+
     Dim conn As New OleDbConnection(Notifications.connectionString)
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    'Public Class NotificationForm Private connString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=HouseholdDB.accdb;"
+    Private Sub NotificationForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadNotifications()
-        UpdateUnreadCount()
-        CheckOverdueChores()
-        'CheckOverdueTasks()
-        CheckInventory()
+        CountUnreadNotifications()
+        Timer1.Interval = 500
+        Timer1.Enabled = False
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim Conn As New OleDbConnection(Notifications.connectionString)
-
-
-        Try
-            ' Open the connection
-            Conn.Open()
-
-            ' Execute the delete query
-            Dim query As String = "DELETE FROM Notifications"
-            Dim cmd As New OleDbCommand(query, Conn)
-            cmd.ExecuteNonQuery()
-
-            ' Reload the list
-            LoadNotifications()
-
-        Catch ex As Exception
-            MessageBox.Show("Error deleting notifications: " & ex.Message)
-
-        Finally
-            ' Close the connection
-            If Conn.State = ConnectionState.Open Then
-                Conn.Close()
-            End If
-        End Try
+        Private Sub LoadNotifications()
+        Dim query As String = "SELECT * FROM Notifications ORDER BY DateCreated DESC"
+        Using conn As New OleDbConnection(Notifications.connectionString), cmd As New OleDbCommand(query, conn)
+            Dim adapter As New OleDbDataAdapter(cmd)
+            Dim table As New DataTable()
+            adapter.Fill(table)
+            DataGridView1.DataSource = table
+        End Using
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim Conn As New OleDbConnection(Notifications.connectionString)
+        Private Sub TrackExpenses()
+        Dim query As String = "INSERT INTO Notifications (UserID, Message, DateCreated, Category, IsRead) " &
+                                  "SELECT 'Expense', 'Recent expense recorded: ' & Amount, Date(), False FROM Expenses WHERE Date >= Date()-7"
+        ExecuteQuery(query)
+        End Sub
 
-        If ListBox1.SelectedIndex <> -1 Then
-            Dim selectedText As String = ListBox1.SelectedItem.ToString()
-            Dim notificationID As Integer = Convert.ToInt32(selectedText.Split(" - ")(0)) ' Extract ID
+        Private Sub TrackOverdueChores()
+        Dim query As String = "INSERT INTO Notifications (UserID, Message, DateCreated, Category, IsRead) " &
+                                  "SELECT 'Chores', 'Overdue chore: ' & ChoreName, DueDate, False FROM Chores WHERE Completed = False AND DueDate < Date()"
+        ExecuteQuery(query)
+        End Sub
 
-            Dim query As String = "UPDATE Notifications SET IsRead=True WHERE NotificationID=@ID"
-            Dim cmd As New OleDbCommand(query, Conn)
-            cmd.Parameters.AddWithValue("@ID", notificationID)
+        Private Sub TrackPendingTasks()
+        Dim query As String = "INSERT INTO Notifications (UserID, Message, DateCreated, Category, IsRead) " &
+                                  "SELECT 'Task', 'Pending task: ' & TaskName, DueDate, False FROM Tasks WHERE Completed = False"
+        ExecuteQuery(query)
+        End Sub
+
+        Private Sub TrackLowInventory()
+        Dim query As String = "INSERT INTO Notifications (UserID, Message, DateCreated, Category, IsRead) " &
+                                  "SELECT 'Inventory', 'Low inventory: ' & ItemName, Date(), False FROM Inventory WHERE Quantity < 5"
+        ExecuteQuery(query)
+        End Sub
+
+        Private Sub ExecuteQuery(query As String)
+        Using conn As New OleDbConnection(Notifications.connectionString), cmd As New OleDbCommand(query, conn)
+            conn.Open()
             cmd.ExecuteNonQuery()
+        End Using
+        LoadNotifications()
+            ShowNewNotifications()
+        End Sub
 
-            LoadNotifications()
-        Else
-            MessageBox.Show("Select a notification to mark as read.")
+        Private Sub ShowNewNotifications()
+            Dim query As String = "SELECT Message FROM Notifications WHERE IsRead = False"
+        Using conn As New OleDbConnection(Notifications.connectionString), cmd As New OleDbCommand(query, conn)
+            conn.Open()
+            Using reader As OleDbDataReader = cmd.ExecuteReader()
+                Dim messages As String = ""
+                While reader.Read()
+                    messages &= reader("Message").ToString() & vbCrLf
+                End While
+                If messages <> "" Then
+                    MessageBox.Show("New Notifications:" & vbCrLf & messages, "Notifications", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End Using
+        End Using
+    End Sub
+
+    'Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+    '    TrackExpenses()
+    '    TrackOverdueChores()
+    '    TrackPendingTasks()
+    '    TrackLowInventory()
+    'End Sub
+
+
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        If e.RowIndex >= 0 Then
+            Dim notificationId As Integer = DataGridView1.Rows(e.RowIndex).Cells("ID").Value
+            Dim query As String = "UPDATE Notifications SET IsRead = True WHERE ID = " & notificationId
+            ExecuteQuery(query)
         End If
     End Sub
 
-    Sub LoadNotifications()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
         Dim Conn As New OleDbConnection(Notifications.connectionString)
 
-        ListBox1.Items.Clear()
+        If DataGridView1.SelectedRows.Count = 0 Then
+            MsgBox("Please select a notification to mark as read.", MsgBoxStyle.Exclamation, "No Selection")
+            Return
+        End If
 
-        Try
-            ' Open Connection
-            If Conn.State = ConnectionState.Closed Then
-                Conn.Open()
-            End If
+        Conn.Open()
 
-            Dim query As String = "SELECT ID, Message FROM Notifications ORDER BY DateCreated DESC"
-            Dim cmd As New OleDbCommand(query, Conn)
-            Dim reader As OleDbDataReader = cmd.ExecuteReader()
+        For Each row As DataGridViewRow In DataGridView1.SelectedRows
+            Dim notificationID As Integer = Convert.ToInt32(row.Cells("ID").Value)
+            Dim query As String = "UPDATE Notifications SET IsRead = True WHERE ID = @ID"
 
-            While reader.Read()
-                Dim notificationID As Integer = reader("NotificationID")
-                Dim message As String = reader("Message")
-                ListBox1.Items.Add(notificationID & " - " & message) ' Display ID & Message
-            End While
+            Using cmd As New OleDbCommand(query, Conn)
+                cmd.Parameters.AddWithValue("@ID", notificationID)
+                cmd.ExecuteNonQuery()
+            End Using
 
-            reader.Close()
-            UpdateUnreadCount()
+            row.DefaultCellStyle.ForeColor = Color.Black ' Change UI for read status
+        Next
 
-        Catch ex As Exception
-            MessageBox.Show("Error loading notifications: " & ex.Message)
+        Conn.Close()
+        MsgBox("Selected notifications marked as read!", MsgBoxStyle.Information, "Updated")
 
-        Finally
-            ' Close Connection
-            If Conn.State = ConnectionState.Open Then
-                Conn.Close()
-            End If
-        End Try
+        ' Refresh unread count after marking as read
+        'CountUnreadNotifications()
+
+
     End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        If DataGridView1.SelectedRows.Count = 0 Then
+            MsgBox("Please select a notification to delete.", MsgBoxStyle.Exclamation, "No Selection")
+            Return
+        End If
+
+        Dim confirmDelete As DialogResult = MessageBox.Show("Are you sure you want to delete the selected notifications?", "Confirm Deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+        If confirmDelete = DialogResult.No Then
+            Return
+        End If
+
+        Using conn As New OleDbConnection(Notifications.connectionString)
+            conn.Open()
+            For Each row As DataGridViewRow In DataGridView1.SelectedRows
+                Dim notificationID As Integer = Convert.ToInt32(row.Cells("ID").Value)
+                Dim query As String = "DELETE FROM Notifications WHERE ID = @ID"
+                Using cmd As New OleDbCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@ID", notificationID)
+                    cmd.ExecuteNonQuery()
+                End Using
+                DataGridView1.Rows.Remove(row)
+            Next
+        End Using
+
+        MsgBox("Selected notifications cleared!", MsgBoxStyle.Information, "Deleted")
+        CountUnreadNotifications()
+    End Sub
+
+
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        UpdateUnreadCount()
+        TrackExpenses()
+        TrackOverdueChores()
+        TrackPendingTasks()
+        TrackLowInventory()
+        Timer1.Interval = 500
+        Timer1.Enabled = False
     End Sub
-
-    Sub UpdateUnreadCount()
-        Dim Conn As New OleDbConnection(Notifications.connectionString)
-
-        Try
-            ' Open Connection
-            Conn.Open()
-
-            Dim query As String = "SELECT COUNT(*) FROM Notifications WHERE IsRead=False"
-            Dim cmd As New OleDbCommand(query, Conn)
+    Private Sub CountUnreadNotifications()
+        Dim query As String = "SELECT COUNT(*) FROM Notifications WHERE IsRead = False"
+        Using conn As New OleDbConnection(Notifications.connectionString), cmd As New OleDbCommand(query, conn)
+            conn.Open()
             Dim count As Integer = Convert.ToInt32(cmd.ExecuteScalar())
-
-            Label2.Text = "Unread: " & count
-
-        Catch ex As Exception
-            MessageBox.Show("Error updating unread count: " & ex.Message)
-
-        Finally
-            ' Close Connection
-            If Conn.State = ConnectionState.Open Then
-                Conn.Close()
-            End If
-        End Try
-    End Sub
-
-    ' Function to check inventory quantity and send notification if below 5
-    Private Sub CheckInventory()
-        Dim conn As New OleDbConnection(Notifications.connectionString)
-        Dim query As String = "SELECT Item, Quantity FROM Inventory WHERE Quantity < 5"
-
-        Using connection As New OleDbConnection(connectionString)
-            Dim command As New OleDbCommand(query, connection)
-            connection.Open()
-
-            Dim reader As OleDbDataReader = command.ExecuteReader()
-
-            ' Check if any items are below the threshold of 5
-            If reader.HasRows Then
-                While reader.Read()
-                    Dim item As String = reader("Item").ToString()
-                    Dim quantity As Integer = Convert.ToInt32(reader("Quantity"))
-
-                    ' Trigger a notification for low stock
-                    If quantity < 5 Then
-                        ' You can use a simple MessageBox for a notification
-                        MessageBox.Show($"Low Inventory Alert: {item} is below 5 units. Current stock: {quantity}", "Low Inventory", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-
-                        ' Alternatively, you can use a custom notification control or system tray notification.
-                        ' NotifyIcon1.ShowBalloonTip(3000, "Low Inventory", $"{item} is below 5 units!", ToolTipIcon.Warning)
-                    End If
-                End While
-            End If
-
-            reader.Close()
+            Label2.Text = "Unread Notifications: " & count.ToString()
         End Using
     End Sub
-
-    ' Function to() check For overdue chores And send a notification
-    Private Sub CheckOverdueChores()
-        Dim conn As New OleDbConnection(Notifications.connectionString)
-        Dim query As String = "SELECT ChoreName, DueDate, AssignedTo FROM Chores WHERE DueDate < @CurrentDate AND Status <> 'Completed'"
-
-        Using connection As New OleDbConnection(connectionString)
-            Dim command As New OleDbCommand(query, connection)
-            command.Parameters.AddWithValue("@CurrentDate", DateTime.Now) ' Current Date
-
-            connection.Open()
-
-            Dim reader As OleDbDataReader = command.ExecuteReader()
-
-            ' Check if there are any overdue chores
-            If reader.HasRows Then
-                While reader.Read()
-                    Dim choreName As String = reader("ChoreName").ToString()
-                    Dim dueDate As DateTime = Convert.ToDateTime(reader("DueDate"))
-                    Dim assignedTo As Integer = Convert.ToInt32(reader("AssignedTo"))
-
-                    ' Trigger overdue notification
-                    Dim message As String = $"Overdue Chore: {choreName} was due on {dueDate.ToShortDateString()}."
-                    SendNotification(message, "Chore", assignedTo)
-                End While
-            End If
-
-            reader.Close()
-        End Using
-
-
-        ' Function to check for overdue tasks and send a notification
-        'Private Sub CheckOverdueTasks()
-        '    Dim conn As New OleDbConnection(Notifications.connectionString)
-        '    Dim query As String = "SELECT TaskName, DueDate, AssignedTo FROM Tasks WHERE DueDate < @CurrentDate AND Status <> 'Completed'"
-
-        '    Using connection As New OleDbConnection(connectionString)
-        '        Dim command As New OleDbCommand(query, connection)
-        '        command.Parameters.AddWithValue("@CurrentDate", DateTime.Now) ' Current Date
-
-        '        connection.Open()
-
-        '        Dim reader As OleDbDataReader = command.ExecuteReader()
-
-        '        ' Check if there are any overdue tasks
-        '        If reader.HasRows Then
-        '            While reader.Read()
-        '                Dim taskName As String = reader("TaskName").ToString()
-        '                Dim dueDate As DateTime = Convert.ToDateTime(reader("DueDate"))
-        '                Dim assignedTo As Integer = Convert.ToInt32(reader("AssignedTo"))
-
-        '                ' Trigger overdue notification
-        '                Dim message As String = $"Overdue Task: {taskName} was due on {dueDate.ToShortDateString()}."
-        '                SendNotification(message, "Task", assignedTo)
-        '            End While
-        '        End If
-
-        '        reader.Close()
-        '    End Using
-    End Sub
-
-    ' Function to add a notification to the database
-    Private Sub SendNotification(message As String, category As String, userId As Integer)
-        Dim connectionString As String = Notifications.connectionString
-        Dim query As String = "INSERT INTO Notifications (Message, DateCreated, Category, UserId) VALUES (@Message, @DateCreated, @Category, @UserId)"
-
-        Using connection As New OleDbConnection(connectionString)
-            Dim command As New OleDbCommand(query, connection)
-            command.Parameters.AddWithValue("@Message", message)
-            command.Parameters.AddWithValue("@DateCreated", DateTime.Now)
-            command.Parameters.AddWithValue("@Category", category)
-            command.Parameters.AddWithValue("@UserId", userId)
-
-            connection.Open()
-            command.ExecuteNonQuery()
-        End Using
-    End Sub
-
 End Class
+
