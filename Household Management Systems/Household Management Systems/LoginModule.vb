ac4890a71de9fe5cfd7cfdaa57d118ffafb64780
Zwivhuya <Zwivhuya@TOMBO>
2025-04-03 10:49:27 +0200

    commit raele

---
+++ Household Management Systems/Household Management Systems/LoginModule.vb
@@ -0,0 +1,9 @@
+﻿Imports System.IO
+Imports System.Data.OleDb
+
+Module LoginModule
+
+    Dim conn As New OleDbConnection(LoginModule.connectionString)
+    Public Const connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Zwivhuya\Desktop\HouseholdManagementSystems\Household Management Systems\Household Management Systems\HMS.accdb"
+
+End Module
