ba058416251419bdd7b8e974722e28efb1fc6b91
Murangi <Murangi@MUDAUMURANGI>
2025-04-04 09:23:15 +0200

    chores

--- Chores Management/Chores Management/Form1.vb
+++ Chores Management/Chores Management/Form1.vb
@@ -62,4 +62,8 @@ Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
