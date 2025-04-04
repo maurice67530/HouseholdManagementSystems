2b5cc6d79aff16942c9647317968f75f671231da
Zwivhuya <Zwivhuya@TOMBO>
2025-04-04 09:11:57 +0200

    commit class

---
+++ Household Management Systems/Household Management Systems/Class1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Class1
+    'properties of the Register class
+    Public Property FirstName As String
+    Public Property LastName As String
+    Public Property Password As String
+    Public Property Username As String
+    Public Property ID As Integer
+    Public Property Email As String
+    Public Property Role As String
+    Public Property DateCreated As String
+End Class
+
+Public Class Login
+    ' Properties of the Login class
+    Public Property UserName As String
+    Public Property Password As String
+
+End Class
