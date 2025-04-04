251bb068250794f874fe1493c83075190bc4d9b0
Rotondwa <Rotondwa@MUKWEVHO>
2025-04-04 12:15:26 +0200

    commiting

--- Notification/Notification/Form1.Designer.vb
+++ Notification/Notification/Form1.Designer.vb
@@ -31,6 +31,7 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.btnClear = New System.Windows.Forms.Button()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -38,7 +39,7 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(3, 71)
+        Me.Label2.Location = New System.Drawing.Point(3, 69)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(166, 16)
         Me.Label2.TabIndex = 10
@@ -48,7 +49,7 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(1, 22)
+        Me.Label1.Location = New System.Drawing.Point(83, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(222, 29)
         Me.Label1.TabIndex = 8
@@ -60,35 +61,46 @@ Partial Class Form1
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(6, 99)
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 99)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(310, 134)
+        Me.DataGridView1.Size = New System.Drawing.Size(373, 134)
         Me.DataGridView1.TabIndex = 11
         '
         'Button1
         '
+        Me.Button1.ForeColor = System.Drawing.SystemColors.InactiveCaption
         Me.Button1.Image = Global.Notification.My.Resources.Resources.icons8_double_tick_32__1_
-        Me.Button1.Location = New System.Drawing.Point(6, 260)
+        Me.Button1.Location = New System.Drawing.Point(6, 258)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(125, 40)
+        Me.Button1.Size = New System.Drawing.Size(102, 40)
         Me.Button1.TabIndex = 6
         Me.Button1.UseVisualStyleBackColor = True
         '
         'btnClear
         '
         Me.btnClear.Image = CType(resources.GetObject("btnClear.Image"), System.Drawing.Image)
-        Me.btnClear.Location = New System.Drawing.Point(179, 260)
+        Me.btnClear.Location = New System.Drawing.Point(137, 258)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(121, 40)
+        Me.btnClear.Size = New System.Drawing.Size(102, 40)
         Me.btnClear.TabIndex = 13
         Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.Location = New System.Drawing.Point(274, 258)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(102, 40)
+        Me.Button2.TabIndex = 14
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.InactiveCaption
-        Me.ClientSize = New System.Drawing.Size(328, 324)
+        Me.ClientSize = New System.Drawing.Size(388, 322)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Label2)
@@ -109,4 +121,5 @@ Partial Class Form1
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents btnClear As Button
     Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents Button2 As Button
 End Class
