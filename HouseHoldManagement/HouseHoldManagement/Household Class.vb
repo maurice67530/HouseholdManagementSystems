4ded6840360f10b23e3c86ee01878fb2b3d5c01b
Zwivhuya <Zwivhuya@TOMBO>
2025-04-08 11:36:07 +0200

    commit mealplan

--- HouseHoldManagement/HouseHoldManagement/Household Class.vb
+++ HouseHoldManagement/HouseHoldManagement/Household Class.vb
@@ -35,14 +35,31 @@ Public Class Logging
 End Class
 
 Public Class chores_
-        Public Property Title As String
-        Public Property AssignedTo As String
-        Public Property Priority As String
-        Public Property Status As String
-        Public Property Frequency As String
-        Public Property DueDate As String
-        Public Property Recurring As String
-        Public Property Description As String
+    Public Property Title As String
+    Public Property AssignedTo As String
+    Public Property Priority As String
+    Public Property Status As String
+    Public Property Frequency As String
+    Public Property DueDate As String
+    Public Property Recurring As String
+    Public Property Description As String
 
 
-    End Class
+End Class
+Public Class MealPlan
+
+    'properties of the mealplan class
+    Public Property MealPlanId As String
+    Public Property StartDate As DateTime
+    Public Property EndDate As DateTime
+    Public Property Meals As String
+    Public Property MealName As String
+    Public Property Items As String
+    Public Property TotalCalories As Integer
+    Public Property Description As String
+    Public Property FilePath As String
+    Public Property Calories As String
+    Public Property Ingredient As String
+    Public Property MealPlanPrint As String
+
+End Class
