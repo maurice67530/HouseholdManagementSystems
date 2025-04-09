4e80adad66d79fd1d0449092d695bafc352bf43c
Mulalo348 <austinmulalo113@gmail.com>
2025-04-09 11:50:03 +0200

    mealPlan

--- HouseHoldManagement/HouseHoldManagement/MealPlan.vb
+++ HouseHoldManagement/HouseHoldManagement/MealPlan.vb
@@ -49,4 +49,87 @@ Public Class MealPlan
         ComboBox2.Items.AddRange(New String() {"Noodles", "Chicken", "Bread"})
         ListBox1.Items.AddRange(New String() {"Noodles", "Chicken Curry", "Kota"})
     End Sub
+    '    Public Function SuggestMeals() As List(Of String)
+    '        Dim suggestedMeals As New List(Of String)
+
+    '        Try
+    '            Using connect As New OleDbConnection(connectionString)
+
+    '            End Using
+    '            conn.Open()
+
+    '            ' Get all meal recipes
+    '            Dim mealQuery As String = "SELECT MealName,Items FROM MealPlans"
+    '            Dim mealCommand As New OleDb.OleDbCommand(mealQuery, conn)
+    '            Dim mealReader As OleDb.OleDbDataReader = mealCommand.ExecuteReader()
+
+    '            While mealReader.Read()
+    '                Dim mealName As String = mealReader("MealName").ToString()
+    '                Dim requiredIngredients As String() = mealReader("Items").ToString().Split(",")
+
+    '                Dim allIngredientsAvailable As Boolean = True
+
+    '                ' Check if all required ingredients exist in GroceryInventory and are not expired
+    '                For Each ingredient In requiredIngredients
+    '                    Dim trimmedIngredient As String = ingredient.Trim()
+    '                    Dim checkQuery As String = "SELECT ExpiryDate FROM GroceryItemss WHERE ItemName=@Ingredients AND Quantity > 0"
+    '                    Dim checkCommand As New OleDb.OleDbCommand(checkQuery, conn)
+    '                    checkCommand.Parameters.AddWithValue("@Ingredients", trimmedIngredient)
+
+    '                    Dim expirationDate As Object = checkCommand.ExecuteScalar()
+
+    '                    ' Check if the ingredient exists and its expiration date
+    '                    If expirationDate Is Nothing Then
+    '                        allIngredientsAvailable = False
+    '                    Else
+    '                        ' Validate that the ingredient is not expired
+    '                        If Convert.ToDateTime(expirationDate) < DateTime.Now Then
+    '                            allIngredientsAvailable = False
+    '                        End If
+    '                    End If
+
+    '                    If Not allIngredientsAvailable Then
+    '                        Exit For
+    '                    End If
+    '                Next
+
+    '                ' If all ingredients are available and not expired, add the meal to suggested list
+    '                If allIngredientsAvailable Then
+    '                    suggestedMeals.Add(mealName)
+    '                End If
+    '            End While
+    '            mealReader.Close()
+
+
+
+    '        Catch ex As Exception
+    '            MsgBox("Error suggesting meals: " & ex.Message, MsgBoxStyle.Critical, "Database Error")
+    '        Finally
+    '            conn.Close()
+    '        End Try
+
+    '        Return suggestedMeals
+    '    End Function
+
+    '    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+    '        'Module1.Mains()
+
+    '        ListBox1.Items.Clear()
+    '        TextBox2.ReadOnly = True
+    '        Dim meals As List(Of String) = SuggestMeals()
+
+    '        If meals.Count > 0 Then
+    '            ForEach meal In meals
+    '               ListBox2.Items.Add(meal)
+
+    '            Next
+    '            MsgBox("Meal Suggestions have been prepared with current Greocery Items.", MsgBoxStyle.Information, "No Available Meals")
+    '            'FetchAlternativeMeals(SuggestMeals)
+    '        Else
+    '            MsgBox("No meals can be prepared with current inventory.", MsgBoxStyle.Exclamation, "No Available Meals")
+    '        End If
+    '        End Su
+    'Private Sub btnSuggest_Click(sender As Object, e As EventArgs) Handles btnSuggest.Click
+
+    '    End Sub
 End Class
