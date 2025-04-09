850e96d2a0dabb0fa6aaf00f97d3719c1de5c138
Zwivhuya <Zwivhuya@TOMBO>
2025-04-09 12:30:53 +0200

    commit

--- HouseHoldManagement/HouseHoldManagement/MealPlan.vb
+++ HouseHoldManagement/HouseHoldManagement/MealPlan.vb
@@ -58,7 +58,43 @@ Public Class MealPlan
         tooltip.SetToolTip(btnSuggest, "Suggest")
         tooltip.SetToolTip(btnFilter, "Filter")
 
+        LoadMealPlanfromDatabase1()
         PopulateDataGridView()
+    End Sub
+
+    Public Sub LoadMealPlanfromDatabase1()
+        Try
+            Debug.WriteLine("Form loading the data")
+            Debug.WriteLine("Form loading  data failed")
+
+            Using conn As New OleDbConnection(HouseHoldManagment_Module.connectionString)
+                conn.Open()
+
+                'Update the table name if neccessary
+                Dim DataTable As String = "MealPlans"
+
+                'Create an OleDbCommand to select the data from the database
+                Dim cmd As New OleDbCommand($"SELECT*FROM {DataTable}", conn)
+
+                'create a DataAdapter and fill a DataTable
+                Dim da As New OleDbDataAdapter(cmd)
+                Dim dt As New DataTable()
+                da.Fill(dt)
+
+                'Bind the DataTable to the DataGridView
+                DataGridView1.DataSource = dt
+            End Using
+
+        Catch ex As FormatException
+            Debug.WriteLine("initializing the data.")
+            MessageBox.Show("Please load the information.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            MessageBox.Show("&Error Loading task to database: " & ex.Message & vbNewLine & ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch ex As Exception
+            Debug.WriteLine($"Form loaded unsuccessfully {ex.Message}")
+            MessageBox.Show("An unexpected error occured during loading the data.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("&unexpected Error:" & ex.Message & vbNewLine & ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
     End Sub
     Public Property meals As New List(Of MealPlans)
     Private Sub PopulateDataGridView()
