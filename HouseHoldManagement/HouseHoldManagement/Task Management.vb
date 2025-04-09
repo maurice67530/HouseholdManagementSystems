42bee2ae90a5945fe1c41fc1c18ed64ff3a77740
Faith <Faith@TSHIWELA>
2025-04-09 12:28:23 +0200

    changes

--- HouseHoldManagement/HouseHoldManagement/Task Management.vb
+++ HouseHoldManagement/HouseHoldManagement/Task Management.vb
@@ -217,4 +217,8 @@ Public Class Task_Management
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
 End Class
