ca4434573ddedf868adf29dc5f5f0d6bac190ec2
Xiluva <Xiluva@XILUVA>
2025-04-10 11:44:19 +0200

    commit

--- HouseHoldManagement/HouseHoldManagement/HouseHoldManagment Module.vb
+++ HouseHoldManagement/HouseHoldManagement/HouseHoldManagment Module.vb
@@ -3,16 +3,16 @@ Imports System.IO
 Imports System.Data.OleDb
 Module HouseHoldManagment_Module
 
-    Public currentUser As String ' Global variable for logged-in user
-    Public Property conn As New OleDbConnection(connectionString)
-    ' Connection string using relative path to the database
-    Public Const connectionString As String = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Xiluva\Source\Repos\maurice67530\HouseholdManagementSystems\HMS.accdb;Persist Security Info=False;"
+    'Public currentUser As String ' Global variable for logged-in user
+    'Public Property conn As New OleDbConnection(connectionString)
+    '' Connection string using relative path to the database
+    ''Public Const connectionString As String = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Xiluva\Source\Repos\maurice67530\HouseholdManagementSystems\HMS.accdb;Persist Security Info=False;"
 
 
-    ' Function to create and return a connection object
-    Public Function GetConnection() As OleDbConnection
-        Return New OleDbConnection(connectionString)
-    End Function
+    '' Function to create and return a connection object
+    'Public Function GetConnection() As OleDbConnection
+    '    Return New OleDbConnection(connectionString)
+    'End Function
 
 End Module
 Module Xiluva
