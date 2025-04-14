024176e0e2868e7efd9d2b572929cc3dd78b7a0f
Murangi <Murangi@MUDAUMURANGI>
2025-04-14 11:08:07 +0200

    JJ

--- HouseHoldManagement/HouseHoldManagement/My Project/Application.Designer.vb
+++ HouseHoldManagement/HouseHoldManagement/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HouseHoldManagement.Personnel
+            Me.MainForm = Global.HouseHoldManagement.PhotoGallery
         End Sub
     End Class
 End Namespace
