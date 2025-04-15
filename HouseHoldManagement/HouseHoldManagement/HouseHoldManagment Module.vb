7305f35b96668cc91e3fd80a94340d9955cfec06
Delicious <Delicious@DELICIOUS>
2025-04-15 09:28:25 +0200

    mm

--- HouseHoldManagement/HouseHoldManagement/HouseHoldManagment Module.vb
+++ HouseHoldManagement/HouseHoldManagement/HouseHoldManagment Module.vb
@@ -196,10 +196,10 @@ Module Masindi
     Public Const connectionString As String = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Masindi\Source\Repos\maurice67530\HouseholdManagementSystems\HMS.accdb;Persist Security Info=False;"
 End Module
 Module khodani
-    Public Property conn As New OleDbConnection(connectionString)
+    Public Property connn As New OleDbConnection(connectionString)
     Public Const connectionString As String = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\khodani\Source\Repos\maurice67530\HouseholdManagementSystems\HMS.accdb;Persist Security Info=False;"
 End Module
 'Module Faith
-'    Public Property conn As New OleDbConnection(connectionString)
+'    Public Property connect As New OleDbConnection(connectionString)
 '     Public Const connectionString As String = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=
 'End Module
