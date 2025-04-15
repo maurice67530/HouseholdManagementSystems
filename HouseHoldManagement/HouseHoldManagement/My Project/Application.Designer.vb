252e3e12b6032ca8faf0e188dd69b931391e373d
Faith <Faith@TSHIWELA>
2025-04-15 11:42:37 +0200

    changes

--- HouseHoldManagement/HouseHoldManagement/My Project/Application.Designer.vb
+++ HouseHoldManagement/HouseHoldManagement/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HouseHoldManagement.Task_Management
+            Me.MainForm = Global.HouseHoldManagement.Login
         End Sub
     End Class
 End Namespace
