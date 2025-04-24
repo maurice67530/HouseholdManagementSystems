2a4fdbc79fd085e34574fad67164c3c30ad0c9b3
Faith <Faith@TSHIWELA>
2025-04-24 09:54:50 +0200

    changes

--- HouseHoldManagement/HouseHoldManagement/Form1.vb
+++ HouseHoldManagement/HouseHoldManagement/Form1.vb
@@ -44,35 +44,35 @@ Public Class Form1
     End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
-        MealPlan.ShowDialog()
+        Grocery.ShowDialog()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        'Groceryy.ShowDialog()
-    End Sub
-
-    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
         Chores.ShowDialog()
     End Sub
 
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TaskManagement.ShowDialog()
+    End Sub
+
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        '  DailyTask.ShowDialog()
+        Inventory.ShowDialog()
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        Expense.ShowDialog()
-    End Sub
-
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         PhotoGallery.ShowDialog()
     End Sub
 
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Notifications.ShowDialog()
+    End Sub
+
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
-        MealPlan.ShowDialog()
+        Personnel.ShowDialog()
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
-        Notifications.ShowDialog()
+        Expense.ShowDialog()
     End Sub
     Private Sub UpdateBudgetStatus()
 
@@ -209,6 +209,7 @@ Public Class Form1
     Private currentPhotoIndex As Integer = 0
 
     Private WithEvents photoTimer As New Timer()
+    Private TaskManagement As Object
 
     Private Sub LoadRecentPhotos()
 
