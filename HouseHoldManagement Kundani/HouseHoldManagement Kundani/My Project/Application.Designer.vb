bd787c0940fa922f0be2932c596fccc779cf5396
Murangi <Murangi@MUDAUMURANGI>
2025-05-12 12:52:58 +0200

    fgtfffff

--- HouseHoldManagement Kundani/HouseHoldManagement Kundani/My Project/Application.Designer.vb
+++ HouseHoldManagement Kundani/HouseHoldManagement Kundani/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HouseHoldManagement_Kundani.Expense
+            Me.MainForm = Global.HouseHoldManagement_Kundani.Budget
         End Sub
     End Class
 End Namespace
