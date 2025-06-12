771a7558cc678d5e65052c19203371c21f633eaa
Murangi <Murangi@MUDAUMURANGI>
2025-06-12 10:19:25 +0200

    usert guide

--- HouseHoldManagement Kundani/HouseHoldManagement Kundani/My Project/Application.Designer.vb
+++ HouseHoldManagement Kundani/HouseHoldManagement Kundani/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HouseHoldManagement_Kundani.About_Us
+            Me.MainForm = Global.HouseHoldManagement_Kundani.Login
         End Sub
     End Class
 End Namespace
